cbd0b5689b7d48972585cf03f16eb93afc39a9ad
MuhammadUzair-98 <59334723+MuhammadUzair-98@users.noreply.github.com>
2024-11-22 20:51:35 +0500

    Add files via upload
    Assignment 1 Question 2 and 2

---
+++ Assignment01/Q1/assignment1.js
@@ -0,0 +1,9 @@
+const fullName = "Muhammad Uzair";
+let age = 30;
+let email = "uzairhussainbest@gmail.com";
+let occupation = "Software Engineer";
+let phoneNumber = "0300-8081555"
+let instituteName = "Jawan Pakistan";
+let education = "BS Software Engineering";
+
+document.write(`Name: ${fullName} <br> Age: ${age} <br> Email: ${email} <br> Occupation: ${occupation} Mobile: ${phoneNumber} <br> Institute Name: ${instituteName} <br> Education: ${education}`)
---
+++ Assignment01/Q1/assignment2.js
@@ -0,0 +1,131 @@
+// Question number 2 //
+
+
+//global//
+
+// var name = "uzair"
+// var me = "hussain"
+
+
+// {
+//     var name = "uzzi"
+//     console.log(name); 
+//     console.log(me); 
+// }
+// console.log(name);
+// console.log(me);
+
+
+//  let name = "uzzi";
+// {
+//     let name = "uzzi";
+//     console.log(name);
+// }
+// console.log(name)
+
+// const name = "uzzi";
+// {
+//     const name = "uzzi";
+//     console.log(name);
+// }
+
+// console.log(name)
+
+
+//function scope//
+
+// function name (){
+//     var name = "uzzi";
+//     console.log(name); 
+// }
+// name()
+
+
+// function name (){
+//     let name = "uzzi";
+//     console.log(name); 
+// }
+// name()
+
+
+// function name (){
+//     const name = " uzzi";
+//     console.log(name); 
+// }
+// name()
+
+
+
+// block scope//
+
+// var name = "uzzi"
+// {
+//      var name = " uzzi bhai";
+//      console.log(name); 
+// }
+// console.log(name)
+
+// let name = "ahsan"
+// {
+//      let name = " uzzi bhai";
+//      console.log(name); 
+// }
+// console.log(name)
+
+// const name = "uzzi bhai"
+// {
+//      const name = "uzzi";
+//      console.log(name); 
+// }
+// console.log(name)
+
+
+//reassgined//
+
+// var name = "uzzi"
+// name = "bhai"
+
+// console.log(name)
+
+// let name = "uzzi"
+// name = "bhai"
+
+// console.log(name)
+
+// const name = "uzzi"
+// name = "bhai"
+
+// console.log(name)
+
+
+
+// Redeclared//
+
+// var name = "uzzi"
+// var name = "bhai"
+
+// console.log(name)
+
+
+// let name = "uzzi"
+// let name = "bhai"
+
+// console.log(name)
+
+// const name = "uzzi"
+// const name = "bhai"
+
+// console.log(name)
+
+
+// hoisting//
+
+// console.log(name)
+// var name = "uzzi"
+
+
+// console.log(name)
+// let name = "uzzi"
+
+// console.log(name)
+// const name = "uzzi"
