397d112d54a26f18ef12802f26e60f9bcd5787b2
MuhammadUzair-98 <59334723+MuhammadUzair-98@users.noreply.github.com>
2024-12-15 20:29:33 +0500

    Add files via upload

---
+++ React one/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <p>cosnile</p>
+    <img src="/public/vite.svg" alt="" />
+  </StrictMode>
+)
